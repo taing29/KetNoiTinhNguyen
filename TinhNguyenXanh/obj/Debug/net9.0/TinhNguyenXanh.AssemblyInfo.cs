3c8d2379b96de65604f11fb5d9166d0d62469119
Tài Nguyễn <taiflunf@gmail.com>
2026-01-28 15:18:36 +0700

    up

--- TinhNguyenXanh/obj/Debug/net9.0/TinhNguyenXanh.AssemblyInfo.cs
+++ TinhNguyenXanh/obj/Debug/net9.0/TinhNguyenXanh.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TinhNguyenXanh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede7230c37f57041bc0d8eb0498c56764789498a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TinhNguyenXanh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TinhNguyenXanh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
